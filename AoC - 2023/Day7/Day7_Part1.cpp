09992a070f69dd53c01b0e4b6b286642987df27d
Bobby <s215818@dtu.dk>
2023-12-13 19:43:21 +0100

    Fixed

--- AoC - 2023/Day7/Day7_Part1.cpp
+++ AoC - 2023/Day7/Day7_Part1.cpp
@@ -1,48 +1,52 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
+#include <regex>
 #include <vector>
-#include <string>
-#include <algorithm>
 #include <unordered_map>
+#include <algorithm>
 
-int getCardStrength(const char card) {
-    if (card == '2') return 2;
-    else if (card == '3') return 3;
-    else if (card == '4') return 4;
-    else if (card == '5') return 5;
-    else if (card == '6') return 6;
-    else if (card == '7') return 7;
-    else if (card == '8') return 8;
-    else if (card == '9') return 9;
-    else if (card == 'T') return 10;
-    else if (card == 'J') return 11;
-    else if (card == 'Q') return 12;
-    else if (card == 'K') return 13;
-    else if (card == 'A') return 14;
-    return -1;
+int get_type(const std::string &hand) {
+    std::unordered_map<char, int> counts;
+    for (char c : hand) {
+        counts[c]++;
+    }
+
+    std::vector<int> sorted_counts;
+    for (const auto &pair : counts) {
+        sorted_counts.push_back(pair.second);
+    }
+    std::sort(sorted_counts.rbegin(), sorted_counts.rend());
+
+    if (sorted_counts[0] == 5) return 6;
+    if (sorted_counts[0] == 4) return 5;
+    if (sorted_counts[0] == 3 && sorted_counts[1] == 2) return 4;
+    if (sorted_counts[0] == 3) return 3;
+    if (sorted_counts[0] == 2 && sorted_counts[1] == 2) return 2;
+    if (sorted_counts[0] == 2) return 1;
+    
+    return 0;
 }
 
-bool compareCards(const std::string card1, const std::string card2) {
-    for (size_t i = 0; i < card1.size() && i < card2.size(); i++) {
-        int strength1 = getCardStrength(card1[i]);
-        int strength2 = getCardStrength(card2[i]);
-
-        std::cout << card1[i] << " " << card2[i] << std::endl;
-        std::cout << strength1 << " " << strength2 << std::endl;    
-
+int compare(const std::pair<std::string, int> &a, const std::pair<std::string, int> &b) {
+    int type_a = get_type(a.first);
+    int type_b = get_type(b.first);
+    
+    std::string cards = "23456789TJQKA";
+    for (size_t i = 0; i < a.first.size(); ++i) {
+        if (a.first[i] == b.first[i]) {
+            continue;
+        }
+        if (type_a > type_b) {
+            return 1;
+        }
+        if (type_a < type_b) {
+            return -1;
+        }
         
+        bool a_wins = (cards.find(a.first[i]) > cards.find(b.first[i]));
+        return (a_wins) ? 1 : -1;
     }
-    return card1.size() < card2.size();
-}
-
-size_t cardStrengthOrder(std::vector<std::string> &cards) {
-    std::vector<std::string> strengthOrder;
-
-    for (size_t i = 0; i < cards.size(); i++) {
-        std::sort(cards[i].begin(), cards[i].end(), compareCards);
-        
-    }
+    return 0;
 }
 
 int main() {
@@ -53,34 +57,31 @@ int main() {
         return 1;
     }
 
+    std::vector<std::pair<std::string, int>> hands;
     std::string line;
-    int lineCount = 1;
-    std::unordered_map<int, std::pair<std::string, int>> rankings;
 
-    while(std::getline(file, line)) {
+    while (std::getline(file, line)) {
         std::istringstream iss(line);
         std::string cardValue, winnings;
         iss >> cardValue >> winnings;
-        
-        std::vector<std::string> cards;
-        cards.push_back(cardValue);
-
-        size_t cardStrength = cardStrengthOrder(cards);
 
         int winningsInt = std::stoi(winnings);
 
-        rankings[lineCount++] = std::make_pair(cardStrength, winningsInt);
+        hands.emplace_back(cardValue, winningsInt);
     }
+
+    std::sort(hands.begin(), hands.end(), [](const auto &a, const auto &b) {
+        return compare(a, b) < 0;
+    });
+
+    int totalWinnings = 0;
+    for (size_t rank = 1; rank <= hands.size(); ++rank) {
+        totalWinnings += rank * hands[rank - 1].second;
+    }
+
+    std::cout << "Result: " << totalWinnings << std::endl;
 
     file.close();
-
-    int total_winnings;
-    for (const auto &pair : rankings) {
-        total_winnings += pair.first * pair.second.second; 
-        std::cout << pair.first << ": " << pair.second.first << " " << pair.second.second << std::endl;
-    }
-
-    std::cout << total_winnings << std::endl;
 
     return 0;
 }
