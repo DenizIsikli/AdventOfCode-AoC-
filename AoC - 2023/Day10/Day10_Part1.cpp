19a1e10f705c2f4e287a27dbffcc095816bc63d7
Bobby <s215818@dtu.dk>
2023-12-17 23:55:30 +0100

    Day 10 part 1 fix

--- AoC - 2023/Day10/Day10_Part1.cpp
+++ AoC - 2023/Day10/Day10_Part1.cpp
@@ -1,78 +1,71 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <string>
 #include <unordered_map>
 #include <unordered_set>
-#include <algorithm>
+#include <string>
 
-struct pair_hash {
-    template <class T1, class T2>
-    std::size_t operator () (const std::pair<T1, T2>& pair) const {
-        return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
-    }
-};
-
-std::unordered_map<char, std::vector<std::pair<int, int>>> directions {
+std::unordered_map<char, std::vector<std::pair<int, int>>> directions = {
     {'|', {{-1, 0}, {1, 0}}},
-    {'-', {{0, -1}, {0, 1}}},
-    {'L', {{-1, 0}, {0, 1}}},
-    {'J', {{-1, 0}, {0, -1}}},
+    {'-', {{0, 1}, {0, -1}}},
+    {'L', {{0, 1}, {-1, 0}}},
+    {'J', {{0, -1}, {-1, 0}}},
     {'7', {{1, 0}, {0, -1}}},
     {'F', {{1, 0}, {0, 1}}},
-    {'S', {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}}
+    {'S', {{-1, 0}, {1, 0}, {0, 1}, {0, -1}}},
+    {'.', {}}
 };
 
-int traverse(std::pair<int, int> current_pos, std::unordered_map<std::pair<int, int>, char, pair_hash> &nodePositions, 
-             std::unordered_map<std::pair<int, int>, int, pair_hash> &distances, int current_distance = 0) {
-    static std::unordered_set<std::pair<int, int>, pair_hash> visited;
-
-    if (visited.find(current_pos) != visited.end()) {
-        return 0;
-    }
-    visited.insert(current_pos);
-
-    int max_distance = current_distance;
-    distances[current_pos] = current_distance;
-
-    for (auto direction : directions[nodePositions[current_pos]]) {
-        std::pair<int, int> new_pos = { current_pos.first + direction.first, current_pos.second + direction.second };
-        if (nodePositions.find(new_pos) != nodePositions.end()) {
-            max_distance = std::max(max_distance, traverse(new_pos, nodePositions, distances, current_distance + 1));
-        }
-    }
-
-    return max_distance;
-}
-
 int main() {
-    std::ifstream file("input.txt");
+    std::ifstream file("Input.txt");
     std::string line;
-    std::vector<std::string> lines;
+    std::unordered_map<int, char> maze;
+    int start = 0;
+    int row = 0;
+    int max_width = 0;
+    int key;
 
     while (std::getline(file, line)) {
-        lines.push_back(line);
+        max_width = line.length();
+
+        for (int col = 0; col < line.length(); ++col) {
+            key++;
+            maze[key] = line[col];
+            if (line[col] == 'S') {
+                start = key;
+            }
+        }
+        ++row;
     }
-    file.close();
 
-    std::unordered_map<std::pair<int, int>, char, pair_hash> nodePositions;
-    std::pair<int, int> current_pos;
-
-    for (int x = 0; x < lines.size(); ++x) {
-        for (int y = 0; y < lines[x].size(); ++y) {
-            char c = lines[x][y];
-            if (c != '.') {
-                nodePositions[{x, y}] = c;
-                if (c == 'S') {
-                    current_pos = {x, y};
-                }
+    std::unordered_map<int, std::unordered_set<int>> graph;
+    for (const auto &m : maze) {
+        int pos = m.first;
+        char c = m.second;
+        int row = pos / max_width;
+        int col = pos % max_width;
+        for (auto &dir: directions[c]) {
+            int neighbor = (row + dir.first) * max_width + (col + dir.second);
+            if (maze.find(neighbor) != maze.end()) {
+                graph[pos].insert(neighbor);
             }
         }
     }
 
-    std::unordered_map<std::pair<int, int>, int, pair_hash> distances;
-    int furthest_node = traverse(current_pos, nodePositions, distances);
-    std::cout << "The furthest node is " << furthest_node << " nodes away." << std::endl;
+    std::unordered_set<int> seen = {start};
+    std::unordered_set<int> todo = graph[start];
 
+    while (!todo.empty()) {
+        int node = *todo.begin();
+        seen.insert(node);
+        todo.erase(node);
+        for (const auto &n : graph[node]) {
+            if (seen.find(n) == seen.end()) {
+                todo.insert(n);
+            }
+        }
+    }
+
+    std::cout << seen.size() / 2 << std::endl;
     return 0;
 }
