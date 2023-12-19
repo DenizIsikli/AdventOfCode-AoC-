7350fe93e2598b18c93bad2951ef7e64c15c6437
Bobby <s215818@dtu.dk>
2023-12-19 17:54:00 +0100

    Fix

--- AoC - 2023/Day11/Day11_Part2.cpp
+++ AoC - 2023/Day11/Day11_Part2.cpp
@@ -5,8 +5,6 @@
 #include <algorithm>
 #include <unordered_map>
 #include <cmath>
-#include <thread>
-#include <mutex>
 
 std::vector<std::string> expandUniverse(const std::vector<std::string> &lines) {
     int rows = lines.size();
@@ -22,23 +20,25 @@ std::vector<std::string> expandUniverse(const std::vector<std::string> &lines) {
         }
     }
     
-    std::vector<std::string> expanded;
-    for (int i = 0; i < rows; ++i) {
-        for (int k = 0; k < 1000000; ++k) {
-            std::string expandedRow;
-            for (int j = 0; j < cols; ++j) {
-                expandedRow += lines[i][j];
-                if (emptyCol[j]) expandedRow += '.';
+    std::vector<std::string> expandedCols;
+    for (const auto &line : lines) {
+        std::string expandedLine;
+        for (int j = 0; j < cols; ++j) {
+            expandedLine += line[j];
+            if (emptyCol[j]) {
+                expandedLine += std::string(1000000, '.');
             }
-            expanded.push_back(expandedRow);
         }
-        
+        expandedCols.push_back(expandedLine);
     }
 
-    for (int k = 0; k < 1000000; ++k) { // Expand columns
-        std::string emptyColString(cols, '.');
-        for (int i = 0; i < rows; ++i) {
-            expanded.push_back(emptyColString);
+    std::vector<std::string> expanded;
+    for (int i = 0; i < rows; ++i) {
+        expanded.push_back(expandedCols[i]);
+        if (emptyRow[i]) {
+            for (int k = 0; k < 1000000; ++k) {
+                expanded.push_back(std::string(expandedCols[i].size(), '.'));
+            }
         }
     }
 
@@ -49,26 +49,10 @@ int manhattanDistance(const std::pair<int, int> &a, const std::pair<int, int> &b
     return std::abs(a.first - b.first) + std::abs(a.second - b.second);
 }
 
-void computeDistances(const std::unordered_map<int, std::pair<int, int>>& galaxyCoords, int start, int end, int &partialTotal) {
-    int partialSum = 0;
-    auto it = galaxyCoords.begin();
-    std::advance(it, start);
-
-    for (int i = start; i < end; ++i) {
-        auto it2 = it;
-        for (int j = i + 1; j < galaxyCoords.size(); ++j) {
-            ++it2;
-            partialSum += manhattanDistance(it->second, it2->second);
-        }
-        ++it;
-    }
-    partialTotal = partialSum;
-}
-
 int main() {
     std::ifstream file("Input.txt");
     std::string line;
-
+    
     std::vector<std::string> lines;
     while (std::getline(file, line)) {
         lines.push_back(line);
@@ -86,23 +70,13 @@ int main() {
         }
     }
 
-    int numThreads = std::thread::hardware_concurrency();
-    std::vector<std::thread> threads;
-    std::vector<int> partialSums(numThreads);
     int totalPathLength = 0;
-
-    for (int i = 0; i < numThreads; ++i) {
-        int start = (i * galaxyCoords.size()) / numThreads;
-        int end = ((i + 1) * galaxyCoords.size()) / numThreads;
-        threads.emplace_back([&, start, end, i]() {
-            computeDistances(galaxyCoords, start, end, std::ref(partialSums[i]));
-        });
-    }
-
-    for (int i = 0; i < numThreads; ++i) {
-        threads[i].join();
-        totalPathLength += partialSums[i];
+    for (int i = 1; i < galaxyNumber; ++i) {
+        for (int j = i + 1; j < galaxyNumber; ++j) {
+            totalPathLength += manhattanDistance(galaxyCoords[i], galaxyCoords[j]);
+        }
     }
 
     std::cout << totalPathLength << std::endl;
 }
+    
