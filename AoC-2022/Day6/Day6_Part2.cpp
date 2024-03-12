6fc57280741ab9ac41bffde60ede7ed6e05acc7c
Bobby <s215818@dtu.dk>
2024-03-12 02:11:30 +0100

    Day 6

---
+++ AoC-2022/Day6/Day6_Part2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <set>
+
+int findFirstMarker(const std::string &input) {
+    for (int i = 0; i <= input.length() - 14; ++i) {
+        std::set<char> uniqueChars(input.begin() + i, input.begin() + i + 14);
+        if (uniqueChars.size() == 14) {
+            return i + 14;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    std::string line;
+    
+    while (getline(std::cin, line)) {
+        int result = findFirstMarker(line);
+
+        if (result != -1) {
+            std::cout << result << std::endl;
+        } else {
+            std::cout << "No marker found" << std::endl;
+        }
+    }
+}
+
